6cac4a63493f9abea4052f4f822beead488f15e2
Jackson Meires <lordjackson@gmail.com>
2020-01-28 11:23:25 -0300

    Add tb turma e atualizações aluno

--- database/backups_sql_bd/db_tai_laravel_2019.sql
+++ database/backups_sql_bd/db_tai_laravel_2019.sql
@@ -2,7 +2,7 @@
 -- Servidor:                     127.0.0.1
 -- Versão do servidor:           5.7.24 - MySQL Community Server (GPL)
 -- OS do Servidor:               Win64
--- HeidiSQL Versão:              10.2.0.5599
+-- HeidiSQL Versão:              10.3.0.5771
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -21,20 +21,22 @@ CREATE TABLE IF NOT EXISTS `alunos` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `nome` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `curso` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `turma` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `turma_id` int(11) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Copiando dados para a tabela db_tai_laravel_2019.alunos: ~4 rows (aproximadamente)
+-- Copiando dados para a tabela db_tai_laravel_2019.alunos: ~7 rows (aproximadamente)
 /*!40000 ALTER TABLE `alunos` DISABLE KEYS */;
-INSERT INTO `alunos` (`id`, `nome`, `curso`, `turma`, `created_at`, `updated_at`) VALUES
-	(1, 'João Paulo França', 'ALIM', 'MEC', NULL, NULL),
-	(2, 'Mariana Ramos Albernaz', 'INFO', 'INFO2', NULL, NULL),
-	(3, 'teste', 'teste', 'teste', '2019-10-29 13:02:25', '2019-10-29 13:02:25'),
-	(4, '1111111\'', 'Redes2', '11', '2019-10-29 13:02:37', '2019-10-29 13:02:37'),
-	(5, 'Geovany', 'Téc INFORMÁTICA', '06', '2019-10-30 14:21:07', '2019-10-30 14:21:07');
+INSERT INTO `alunos` (`id`, `nome`, `curso`, `turma_id`, `created_at`, `updated_at`) VALUES
+	(1, 'João Paulo França', 'ALIM', 1, NULL, NULL),
+	(2, 'Mariana Ramos Albernaz', 'INFO', 1, NULL, NULL),
+	(3, 'teste', 'teste', 2, '2019-10-29 13:02:25', '2019-10-29 13:02:25'),
+	(4, '1111111\'', 'Redes2', 1, '2019-10-29 13:02:37', '2019-10-29 13:02:37'),
+	(5, 'Geovany', 'Téc INFORMÁTICA', 2, '2019-10-30 14:21:07', '2019-10-30 14:21:07'),
+	(6, 'teste2', '22', 2, '2019-11-26 03:38:34', '2019-11-26 03:38:34'),
+	(7, 'teste', 'test', 2, '2020-01-28 13:10:58', '2020-01-28 13:10:58');
 /*!40000 ALTER TABLE `alunos` ENABLE KEYS */;
 
 -- Copiando estrutura para tabela db_tai_laravel_2019.curso_models
@@ -45,13 +47,12 @@ CREATE TABLE IF NOT EXISTS `curso_models` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Copiando dados para a tabela db_tai_laravel_2019.curso_models: ~4 rows (aproximadamente)
+-- Copiando dados para a tabela db_tai_laravel_2019.curso_models: ~3 rows (aproximadamente)
 /*!40000 ALTER TABLE `curso_models` DISABLE KEYS */;
 INSERT INTO `curso_models` (`id`, `nome`, `abreviatura`) VALUES
 	(1, 'TÉCNICO EM MÉCANICA - MÉDIO INTEGRADO', 'MEC'),
 	(2, 'TÉCNICO EM INFORMÁTICA - MÉDIO INTEGRADO', 'INFO'),
-	(3, 'DESENVOLVIMENTO MÓVEL', 'DEV-MOBILE'),
-	(4, 'TEST', 'TESTE');
+	(3, 'DESENVOLVIMENTO MÓVEL', 'DEV-MOBILE');
 /*!40000 ALTER TABLE `curso_models` ENABLE KEYS */;
 
 -- Copiando estrutura para tabela db_tai_laravel_2019.migrations
@@ -82,6 +83,24 @@ CREATE TABLE IF NOT EXISTS `password_resets` (
 -- Copiando dados para a tabela db_tai_laravel_2019.password_resets: ~0 rows (aproximadamente)
 /*!40000 ALTER TABLE `password_resets` DISABLE KEYS */;
 /*!40000 ALTER TABLE `password_resets` ENABLE KEYS */;
+
+-- Copiando estrutura para tabela db_tai_laravel_2019.turma
+CREATE TABLE IF NOT EXISTS `turma` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(80) DEFAULT NULL,
+  `descricao` varchar(150) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+-- Copiando dados para a tabela db_tai_laravel_2019.turma: ~3 rows (aproximadamente)
+/*!40000 ALTER TABLE `turma` DISABLE KEYS */;
+INSERT INTO `turma` (`id`, `nome`, `descricao`, `created_at`, `updated_at`) VALUES
+	(1, 'INFO06', 'Teste', NULL, NULL),
+	(2, 'MEC03', 'teste', NULL, NULL),
+	(3, 'ALIM02', 'Técnico em Alimentos', '2020-01-28 13:50:46', '2020-01-28 13:50:54');
+/*!40000 ALTER TABLE `turma` ENABLE KEYS */;
 
 -- Copiando estrutura para tabela db_tai_laravel_2019.users
 CREATE TABLE IF NOT EXISTS `users` (
